4193932e2b0aafde3591f2134e4ea9a15942aa7a
gche <cuhkgordon@gmail.com>
2026-06-03 19:18:58 +1200

    Change Description of Data Files

--- R/data.R
+++ R/data.R
@@ -6,26 +6,26 @@
 #' @format A data frame with 1000 rows and 19 variables:
 #' \describe{
 #'   \item{ID}{Numeric, case ID number.}
-#'   \item{trust.1}{Numeric, Average of 6 Trust items at Time 1: from 1 (strongly disagree) to 5 (strongly agree).}
-#'   \item{trust.2}{Numeric, Average of 6 Trust items at Time 2: from 1 (strongly disagree) to 5 (strongly agree).}
-#'   \item{trust.3}{Numeric, Average of 6 Trust items at Time 3: from 1 (strongly disagree) to 5 (strongly agree).}
-#'   \item{trust.4}{Numeric, Average of 6 Trust items at Time 4: from 1 (strongly disagree) to 5 (strongly agree).}
-#'   \item{trust.5}{Numeric, Average of 6 Trust items at Time 5: from 1 (strongly disagree) to 5 (strongly agree).}
-#'   \item{trust.6}{Numeric, Average of 6 Trust items at Time 6: from 1 (strongly disagree) to 5 (strongly agree).}
-#'   \item{lonely.1}{Numeric, Average of 6 UCLS Loneliness Scale at Time 1: from 1 (never) to 4 (always).}
-#'   \item{lonely.2}{Numeric, Average of 6 UCLS Loneliness Scale at Time 2: from 1 (never) to 4 (always).}
-#'   \item{lonely.3}{Numeric, Average of 6 UCLS Loneliness Scale at Time 3: from 1 (never) to 4 (always).}
-#'   \item{lonely.4}{Numeric, Average of 6 UCLS Loneliness Scale at Time 4: from 1 (never) to 4 (always).}
-#'   \item{lonely.5}{Numeric, Average of 6 UCLS Loneliness Scale at Time 5: from 1 (never) to 4 (always).}
-#'   \item{lonely.6}{Numeric, Average of 6 UCLS Loneliness Scale at Time 6: from 1 (never) to 4 (always).}
-#'   \item{lifesat.1}{Numeric, Average of 5 Satisfaction with Life Scale at Time 1: from 1 (strongly disagree) to 7 (strongly agree).}
-#'   \item{lifesat.2}{Numeric, Average of 5 Satisfaction with Life Scale at Time 2: from 1 (strongly disagree) to 7 (strongly agree).}
-#'   \item{lifesat.3}{Numeric, Average of 5 Satisfaction with Life Scale at Time 3: from 1 (strongly disagree) to 7 (strongly agree).}
-#'   \item{lifesat.4}{Numeric, Average of 5 Satisfaction with Life Scale at Time 4: from 1 (strongly disagree) to 7 (strongly agree).}
-#'   \item{lifesat.5}{Numeric, Average of 5 Satisfaction with Life Scale at Time 5: from 1 (strongly disagree) to 7 (strongly agree).}
-#'   \item{lifesat.6}{Numeric, Average of 5 Satisfaction with Life Scale at Time 6: from 1 (strongly disagree) to 7 (strongly agree).}
+#'   \item{trust_T1}{Numeric, Average of 6 Trust items at Time 1: from 1 (strongly disagree) to 5 (strongly agree).}
+#'   \item{trust_T2}{Numeric, Average of 6 Trust items at Time 2: from 1 (strongly disagree) to 5 (strongly agree).}
+#'   \item{trust_T3}{Numeric, Average of 6 Trust items at Time 3: from 1 (strongly disagree) to 5 (strongly agree).}
+#'   \item{trust_T4}{Numeric, Average of 6 Trust items at Time 4: from 1 (strongly disagree) to 5 (strongly agree).}
+#'   \item{trust_T5}{Numeric, Average of 6 Trust items at Time 5: from 1 (strongly disagree) to 5 (strongly agree).}
+#'   \item{trust_T6}{Numeric, Average of 6 Trust items at Time 6: from 1 (strongly disagree) to 5 (strongly agree).}
+#'   \item{lonely_T1}{Numeric, Average of 6 UCLS Loneliness Scale at Time 1: from 1 (never) to 4 (always).}
+#'   \item{lonely_T2}{Numeric, Average of 6 UCLS Loneliness Scale at Time 2: from 1 (never) to 4 (always).}
+#'   \item{lonely_T3}{Numeric, Average of 6 UCLS Loneliness Scale at Time 3: from 1 (never) to 4 (always).}
+#'   \item{lonely_T4}{Numeric, Average of 6 UCLS Loneliness Scale at Time 4: from 1 (never) to 4 (always).}
+#'   \item{lonely_T5}{Numeric, Average of 6 UCLS Loneliness Scale at Time 5: from 1 (never) to 4 (always).}
+#'   \item{lonely_T6}{Numeric, Average of 6 UCLS Loneliness Scale at Time 6: from 1 (never) to 4 (always).}
+#'   \item{lifesat_T1}{Numeric, Average of 5 Satisfaction with Life Scale at Time 1: from 1 (strongly disagree) to 7 (strongly agree).}
+#'   \item{lifesat_T2}{Numeric, Average of 5 Satisfaction with Life Scale at Time 2: from 1 (strongly disagree) to 7 (strongly agree).}
+#'   \item{lifesat_T3}{Numeric, Average of 5 Satisfaction with Life Scale at Time 3: from 1 (strongly disagree) to 7 (strongly agree).}
+#'   \item{lifesat_T4}{Numeric, Average of 5 Satisfaction with Life Scale at Time 4: from 1 (strongly disagree) to 7 (strongly agree).}
+#'   \item{lifesat_T5}{Numeric, Average of 5 Satisfaction with Life Scale at Time 5: from 1 (strongly disagree) to 7 (strongly agree).}
+#'   \item{lifesat_T6}{Numeric, Average of 5 Satisfaction with Life Scale at Time 6: from 1 (strongly disagree) to 7 (strongly agree).}
 #' }
-#' @source Simulated dataset (1000 observations) based on Chen, Y., Fang, Y., Yang, Y., & Dong, Y. (2026) Longitudinal associations among trust, loneliness, and life satisfaction among university students: A between- and within-person analysis.Journal of Personality, 94: 151-162. 
+#' @source Simulated dataset (500 observations) based on Chen, Y., Fang, Y., Yang, Y., & Dong, Y. (2026) Longitudinal associations among trust, loneliness, and life satisfaction among university students: A between- and within-person analysis.Journal of Personality, 94: 151-162. 
 "Trust"
 
 
@@ -37,18 +37,18 @@
 #' @format A data frame with 500 rows and 13 variables:
 #' \describe{
 #'   \item{id}{Numeric, case id number.}
-#'   \item{expose.1}{Numeric, Perceived exposure to smoking in movies at Time 1.}
-#'   \item{expose.2}{Numeric, Perceived exposure to smoking in movies at Time 2.}
-#'   \item{expose.3}{Numeric, Perceived exposure to smoking in movies at Time 3.}
-#'   \item{expose.4}{Numeric, Perceived exposure to smoking in movies at Time 4.}
-#'   \item{expose.5}{Numeric, Perceived exposure to smoking in movies at Time 5.}
-#'   \item{expose.6}{Numeric, Perceived exposure to smoking in movies at Time 6.}
-#'   \item{intens.1}{Numeric, Smoking intensity at Time 1.}
-#'   \item{intens.2}{Numeric, Smoking intensity at Time 2.}
-#'   \item{intens.3}{Numeric, Smoking intensity at Time 3.}
-#'   \item{intens.4}{Numeric, Smoking intensity at Time 4.}
-#'   \item{intens.5}{Numeric, Smoking intensity at Time 5.}
-#'   \item{intens.6}{Numeric, Smoking intensity at Time 6.}
+#'   \item{expose_T1}{Numeric, Perceived exposure to smoking in movies at Time 1.}
+#'   \item{expose_T2}{Numeric, Perceived exposure to smoking in movies at Time 2.}
+#'   \item{expose_T3}{Numeric, Perceived exposure to smoking in movies at Time 3.}
+#'   \item{expose_T4}{Numeric, Perceived exposure to smoking in movies at Time 4.}
+#'   \item{expose_T5}{Numeric, Perceived exposure to smoking in movies at Time 5.}
+#'   \item{expose_T6}{Numeric, Perceived exposure to smoking in movies at Time 6.}
+#'   \item{intens_T1}{Numeric, Smoking intensity at Time 1.}
+#'   \item{intens_T2}{Numeric, Smoking intensity at Time 2.}
+#'   \item{intens_T3}{Numeric, Smoking intensity at Time 3.}
+#'   \item{intens_T4}{Numeric, Smoking intensity at Time 4.}
+#'   \item{intens_T5}{Numeric, Smoking intensity at Time 5.}
+#'   \item{intens_T6}{Numeric, Smoking intensity at Time 6.}
 #' }
 #' @source Dataset with 500 observations across six time points simulated with lavaan::simulateData() based on results of the Minnesota Adolescent Community Cohort (MACC) Study, 2000-2013, reported in Usami, S., Murayama, K., & Hamaker, E. L. (2019). A unified framework of longitudinal models to examine reciprocal relations. Psychological Methods, 24(5), 637-657.
 "Smoking"
